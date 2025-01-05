f32a761b47a3bbd2e54f1b83d23b51504d53a630
RISHAV <rishavsaha24042003@gmail.com>
2025-01-06 00:49:00 +0530

    Added lombok implementation

--- journalApp/src/main/java/com/rishav/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/rishav/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.rishav.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,42 +8,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
+    @Id
+    private LocalDateTime date;
     private ObjectId id;
     private String title;
     private String content;
-    private LocalDateTime date;
-
-    @Id
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
